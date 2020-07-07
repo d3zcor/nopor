5c54d2d4180d0bc956700a01652fceba91d981ed
d3zcor <67931502+d3zcor@users.noreply.github.com>
2020-07-07 09:16:43 -0300

    Add files via upload

---
+++ nxpor.py
@@ -0,0 +1,112 @@
+import webbrowser
+import os
+import time
+
+ 
+print("""
+███████████████████████████████████
+█▄─▄▄▀█▄▄▄░█░▄▄░▄█─▄▄▄─█─▄▄─█▄─▄▄▀█
+██─██─██▄▄░██▀▄█▀█─███▀█─██─██─▄─▄█
+▀▄▄▄▄▀▀▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▀▄▄▀
+    """)
+
+time.sleep(0.90)
+
+     
+print("""
+
+1)xvideos
+
+2)Serviporno
+
+3)Porhub """)
+
+print()
+pagina = int(input('Ingrese el numero de la pagina porno : '))
+
+
+if pagina == 1 :
+   print ("""██╗░░██╗██╗░░░██╗██╗██████╗░███████╗░█████╗░░██████╗
+╚██╗██╔╝██║░░░██║██║██╔══██╗██╔════╝██╔══██╗██╔════╝
+░╚███╔╝░╚██╗░██╔╝██║██║░░██║█████╗░░██║░░██║╚█████╗░
+░██╔██╗░░╚████╔╝░██║██║░░██║██╔══╝░░██║░░██║░╚═══██╗
+██╔╝╚██╗░░╚██╔╝░░██║██████╔╝███████╗╚█████╔╝██████╔╝
+╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═════╝░╚══════╝░╚════╝░╚═════╝░
+         """)
+   print()
+   #webbrowser.open_new_tab('https://www.xvideos.com/')
+   print ("""
+   1)Casero  
+   2)Madre e hijo 
+   3)Patas
+   4)Si no esta su categoria reemplaze los espacios por +
+          """)
+   pagina1 = int(input('Que categoria quiere ver : '))
+   if pagina1 == 1 :
+       webbrowser.open_new_tab('https://www.xvideos.com/?k=casero')
+   if pagina1 == 2 :
+     webbrowser.open_new_tab('https://www.xvideos.com/?k=madre+e+hijo')
+   if pagina1 == 3 :
+      webbrowser.open_new_tab('https://www.xvideos.com/?k=patas')
+   if pagina1 == 4 :
+       buscador = input(' Ingrese la categoria que desea ver , no se olvide de reemplazar los espacios con + : ')
+       webbrowser.open_new_tab('https:www.xvideos.com/?k='+buscador)
+      
+#--------------------------------------------------------------------------
+if pagina == 2 :
+   print("""░██████╗███████╗██████╗░██╗░░░██╗██╗██████╗░░█████╗░██████╗░███╗░░██╗░█████╗░
+██╔════╝██╔════╝██╔══██╗██║░░░██║██║██╔══██╗██╔══██╗██╔══██╗████╗░██║██╔══██╗
+╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝██║██████╔╝██║░░██║██████╔╝██╔██╗██║██║░░██║
+░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██║██╔═══╝░██║░░██║██╔══██╗██║╚████║██║░░██║
+██████╔╝███████╗██║░░██║░░╚██╔╝░░██║██║░░░░░╚█████╔╝██║░░██║██║░╚███║╚█████╔╝
+╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░""")
+   print()
+   print("""
+   1)madre e hijo
+   2)casero
+   3)patas
+   4)otra categoria""")
+   print() 
+   if pagina1 == 1 :
+      webbrowser.open_new_tab('https://www.serviporno.com/search/?q=madre+e+hijo')
+   if pagina1 == 2 : 
+      webbrowser.open_new_tab('https://www.serviporno.com/search/?q=casero')
+   if pagina1 == 3 :
+      webbrowser.open_new_tab('https://www.serviporno.com/search/?q=patas')
+   if pagina1 == 4 :
+      rarito = input('Ingrese su categoria no olvide reemplazar los espacios por +')
+      webbrowser.open_new_tab('https://www.serviporno.com/search/?q='+rarito)
+
+if pagina == 3 :
+    print ("""██████╗░░█████╗░██████╗░██╗░░██╗██╗░░░██╗██████╗░
+██╔══██╗██╔══██╗██╔══██╗██║░░██║██║░░░██║██╔══██╗
+██████╔╝██║░░██║██████╔╝███████║██║░░░██║██████╦╝
+██╔═══╝░██║░░██║██╔══██╗██╔══██║██║░░░██║██╔══██╗
+██║░░░░░╚█████╔╝██║░░██║██║░░██║╚██████╔╝██████╦╝
+╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░""")
+    print ("""
+    1)madre e hijo
+    2)casero
+    3)patas
+    4)otra categoria""")
+    print()
+    pagina1 = int(input(' Ingrese la categoria a ver : '))
+    if pagina1 == 1 :
+      webbrowser.open_new_tab('https://es.pornhub.com/video/search?search=casero')
+    if pagina1 == 2 :
+      webbrowser.open_new_tab('https://es.pornhub.com/video/search?search=madre+e+hijo')
+    if pagina1 == 3 :
+      webbrowser.open_new_tab('https://es.pornhub.com/video/search?search=patas')
+    if pagina1 == 4 :
+      raro = input('Ingrese la categoria a ver no se olvide de reemplazar los espacion por +')
+      webbrowser.open_new_tab('https://es.pornhub.com/video/search?search='+raro)
+
+
+
+
+
+
+
+
+
+
